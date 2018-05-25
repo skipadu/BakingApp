e1e1c0ed7fa01d6e9197ba994d81e665b1478eeb
Sami Korpela <skipadu@users.noreply.github.com>
2018-05-25 23:11:27 +0300

    Drafting the Recipe's model

---
+++ app/src/main/java/com/pihrit/bakingapp/model/Recipe.java
@@ -0,0 +1,43 @@
+package com.pihrit.bakingapp.model;
+
+public class Recipe {
+    private long id;
+    private String name;
+    // TODO
+    // List<RecipeIngredient> ingredients;
+    // List<RecipeStep> steps;
+    private int servings;
+    private String image;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
