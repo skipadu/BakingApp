cf621940baec50c853433a8ff6a28c816cf78aec
Sami Korpela <skipadu@users.noreply.github.com>
2018-05-28 20:54:06 +0300

    Generated classes to match the JSON
    Used this [tool](https://github.com/robohorse/RoboPOJOGenerator) to generate the POJOs from the given .json

--- app/src/main/java/com/pihrit/bakingapp/model/Recipe.java
+++ app/src/main/java/com/pihrit/bakingapp/model/Recipe.java
@@ -1,43 +1,87 @@
 package com.pihrit.bakingapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Recipe {
-    private long id;
-    private String name;
-    // TODO
-    // List<RecipeIngredient> ingredients;
-    // List<RecipeStep> steps;
-    private int servings;
+
+    @SerializedName("image")
     private String image;
 
-    public long getId() {
-        return id;
-    }
+    @SerializedName("servings")
+    private int servings;
 
-    public void setId(long id) {
-        this.id = id;
-    }
+    @SerializedName("name")
+    private String name;
 
-    public String getName() {
-        return name;
-    }
+    @SerializedName("ingredients")
+    private List<IngredientsItem> ingredients;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    @SerializedName("id")
+    private int id;
 
-    public int getServings() {
-        return servings;
-    }
+    @SerializedName("steps")
+    private List<StepsItem> steps;
 
-    public void setServings(int servings) {
-        this.servings = servings;
+    public void setImage(String image) {
+        this.image = image;
     }
 
     public String getImage() {
         return image;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIngredients(List<IngredientsItem> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<IngredientsItem> getIngredients() {
+        return ingredients;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setSteps(List<StepsItem> steps) {
+        this.steps = steps;
+    }
+
+    public List<StepsItem> getSteps() {
+        return steps;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Recipe{" +
+                        "image = '" + image + '\'' +
+                        ",servings = '" + servings + '\'' +
+                        ",name = '" + name + '\'' +
+                        ",ingredients = '" + ingredients + '\'' +
+                        ",id = '" + id + '\'' +
+                        ",steps = '" + steps + '\'' +
+                        "}";
     }
 }
