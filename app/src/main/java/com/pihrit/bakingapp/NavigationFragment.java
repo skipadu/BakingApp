19e248d7feed66293fb5dba604a16408f6720969
Sami Korpela <skipadu@users.noreply.github.com>
2018-05-31 21:17:20 +0300

    Added clicklistener for next/previous buttons in navigationFragment

--- app/src/main/java/com/pihrit/bakingapp/NavigationFragment.java
+++ app/src/main/java/com/pihrit/bakingapp/NavigationFragment.java
@@ -2,12 +2,23 @@ package com.pihrit.bakingapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 // TODO: Actually navigate to previous & next steps
 public class NavigationFragment extends Fragment {
+    @BindView(R.id.btn_previous)
+    public Button previousButton;
+
+    @BindView(R.id.btn_next)
+    public Button nextButton;
 
     private int mStepIndex;
 
@@ -18,10 +29,24 @@ public class NavigationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_navigation, container, false);
+        View v = inflater.inflate(R.layout.fragment_navigation, container, false);
+        ButterKnife.bind(this, v);
+        return v;
     }
 
     public void setStepIndex(int stepIndex) {
         this.mStepIndex = stepIndex;
+    }
+
+    @OnClick(R.id.btn_next)
+    public void goToNextIfPossible() {
+        Log.v("TEST", "Trying to show next view");
+        // TODO: if last, go back to recipe selection?
+    }
+
+    @OnClick(R.id.btn_previous)
+    public void goToPreviousIfPossible() {
+        Log.v("TEST", "Trying to show previous view");
+        // TODO: if first, then go to recipe selection?
     }
 }
